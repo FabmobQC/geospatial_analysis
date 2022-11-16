308ac6c18d7fad383dbd0f6e8098deecc2fed80c
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2022-11-16 11:15:04 +0100

    Updated to suport different inputs

--- README.md
+++ README.md
@@ -2,3 +2,27 @@
 
 ## Reférence
 https://turfpy.readthedocs.io/en/latest/measurements/points_within_polygon.html
+
+
+## Install environment
+
+First create a conda environement
+
+`conda create -n geospatial_analysis`
+
+Then activate the environment
+
+`conda activate geospatial_analysis`
+
+Install dependencies using pip
+
+`pip install -r requirements.txt`
+
+## How to use
+
+`python3 main.py -s signalisation_stationnement.geojson -b zone_zfe_vieux_montreal.geojson -o zone_zfe_vieux_montreal_data.geojson`
+
+You can also filter by `NOM_ARROND` using `-f` flag followed by the exact value from the feature field ['properties']['NOM_ARROND']
+
+Ex:
+`python3 main.py -s signalisation_stationnement.geojson -b zone_zfe_vieux_montreal.geojson -o zone_zfe_vieux_montreal_data.geojson -f 'Rosemont - La Petite-Patrie'`
--- main.py
+++ main.py
@@ -3,40 +3,60 @@ from turfpy.measurement import points_within_polygon
 from geojson import Feature, FeatureCollection, Polygon
 import json
 from ipyleaflet import Map, GeoJSON
+import argparse
+import sys
 
 
-bounderies_file = open("plaza_rosemont.geojson", "r")
-stationnements_file = open("signalisation_stationnement.geojson", "r")
-
-bounderies_data = bounderies_file.read()
-bounderies = json.loads(bounderies_data)
-
-stationnements_data = stationnements_file.read()
-stationnements = json.loads(stationnements_data)
-
-stationnements_plaza = [x for x in stationnements["features"] if x['properties']['NOM_ARROND'] == 'Rosemont - La Petite-Patrie']
-
-points = FeatureCollection(stationnements_plaza)
-
-result = points_within_polygon(points, bounderies["features"][0]["geometry"])
-
-output = open("panneaux_plaza.geojson", "w")
-
-output.write(json.dumps(result, indent=4))
+# m = Map(center=(45.5387105,-73.6116478), zoom=13)
 
 
-output.close()
+# geo_json = GeoJSON(
+#     data=result,
+#     style={"opacity": 1, "dashArray": "9", "fillOpacity": 0.3, "weight": 1},
+#     hover_style={"color": "green", "dashArray": "0", "fillOpacity": 0.5},
+# )
 
-print(len(result["features"]))
+# m.add_layer(geo_json)
+# m
 
-m = Map(center=(45.5387105,-73.6116478), zoom=13)
 
+def main():
+    parser = argparse.ArgumentParser()
 
-geo_json = GeoJSON(
-    data=result,
-    style={"opacity": 1, "dashArray": "9", "fillOpacity": 0.3, "weight": 1},
-    hover_style={"color": "green", "dashArray": "0", "fillOpacity": 0.5},
-)
+    parser.add_argument("-s", "--signalec", help="Path to Signalec data file from Montreal Open data", required=True)
+    parser.add_argument("-b", "--bounderies", help="Path to bounderies polygon file", required=True)
+    parser.add_argument("-o", "--output", help="Path to output geojson file", required=True)
+    parser.add_argument("-f", "--arrondissement", help="Filter input Data by arrondissement", required=False)
 
-m.add_layer(geo_json)
-m
+    args = parser.parse_args()
+
+    if args.signalec and args.bounderies:
+
+        bounderies_file = open(args.bounderies, "r")
+        stationnements_file = open(args.signalec, "r")
+
+        bounderies_data = bounderies_file.read()
+        bounderies = json.loads(bounderies_data)
+
+        stationnements_data = stationnements_file.read()
+        stationnements = json.loads(stationnements_data)
+
+        stationnements_filtered = stationnements
+
+        if args.arrondissement:
+            stationnements_filtered = [x for x in stationnements["features"] if x['properties']['NOM_ARROND'] == args.arrondissement]
+
+        points = FeatureCollection(stationnements_filtered)
+
+        result = points_within_polygon(points, bounderies["features"][0]["geometry"])
+
+        output = open(args.output, "w")
+
+        output.write(json.dumps(result, indent=4))
+
+        output.close()
+
+        print(len(result["features"]))
+
+if __name__ == '__main__':
+    sys.exit(main())    
